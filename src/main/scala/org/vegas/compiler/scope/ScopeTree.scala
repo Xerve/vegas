c7b3e3b80c51b057d70cbe7fbe2b247aa85455f6
Ryan Delaney <rrdelaney@outlook.com>
2015-06-03 17:46:59 -0400

    Started scope tree

--- src/main/scala/org/vegas/compiler/scope/ScopeTree.scala
+++ src/main/scala/org/vegas/compiler/scope/ScopeTree.scala
@@ -1,5 +1,18 @@
 package org.vegas.compiler.scope
 
-class ScopeTree {
+import scala.collection.immutable.{Seq, Map}
 
+type Scope = Seq[String]
+
+case class ScopeNode(val children: Seq[ScopeNode], val nodes: Map[String, String]) {
+    def apply(node: Scope) =
+        nodes.get(node.head) match {
+            case Some(t) => t
+            case None => children.foldLeft(None) { (prev, scope) =>
+                prev match {
+                    case Some(t) => prev
+                    case None => scope(node.tail)
+                }
+            }
+        }
 }
