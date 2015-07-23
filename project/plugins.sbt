cc5a67ebaa10aa3c83f46c990f04210e68542d80
Ryan Delaney <rrdelaney@outlook.com>
2015-07-23 01:20:15 -0400

    Coverage set up

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,3 +1,5 @@
 logLevel := Level.Warn
+
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.1.0")
+addSbtPlugin("com.codacy" % "sbt-codacy-coverage" % "1.1.0")
 addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
--- src/main/scala/org/vegas/prelude/VNumberPrelude.scala
+++ src/main/scala/org/vegas/prelude/VNumberPrelude.scala
@@ -11,7 +11,39 @@ package object VNumberPrelude extends Prelude {
             }
     }
 
+    object VNumberMinus extends VMacro("-") {
+        def eval(callee: Expression, args: Seq[Expression]) =
+            require(args, Seq(VNumber)) {
+                GenericExpression(VNumber, callee.eval + " - " + args.map(_.eval).mkString(" - "))
+            }
+    }
+
+    object VNumberMultiply extends VMacro("*") {
+        def eval(callee: Expression, args: Seq[Expression]) =
+            require(args, Seq(VNumber)) {
+                GenericExpression(VNumber, callee.eval + " * " + args.map(_.eval).mkString(" * "))
+            }
+    }
+
+    object VNumberDivide extends VMacro("/") {
+        def eval(callee: Expression, args: Seq[Expression]) =
+            require(args, Seq(VNumber)) {
+                GenericExpression(VNumber, "(" + callee.eval + " / " + args.map(_.eval).mkString(" / ") + ")")
+            }
+    }
+
+    object VNumberPower extends VMacro("**") {
+        def eval(callee: Expression, args: Seq[Expression]) =
+            require(args, Seq(VNumber)) {
+                GenericExpression(VNumber, "pow(" + callee.eval + ", " + args.map(_.eval).mkString(", ") + ")")
+            }
+    }
+
     def init {
         VNumber define VNumberPlus
+        VNumber define VNumberMinus
+        VNumber define VNumberMultiply
+        VNumber define VNumberDivide
+        VNumber define VNumberPower
     }
 }
